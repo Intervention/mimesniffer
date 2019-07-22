0f6bfc5fdbbff0ebc96e2c58e690d880fc54a63f
Oliver Vogel <oliver@olivervogel.com>
2019-07-22 20:15:55 +0200

    Added Tar Type

--- src/AbstractType.php
+++ src/AbstractType.php
@@ -49,6 +49,7 @@ abstract class AbstractType
         return in_array($this->name, [
             'application/zip',
             'application/x-rar-compressed',
+            'application/x-tar',
         ]);
     }
 
